a6ee2d2c5635edca53cbfffd0b3aee5caf36d118
rafi <raficse30@gmail.com>
2023-11-05 23:57:16 +0600

    Modification of GP for Symbolic Regression

---
+++ model/__init__.py
@@ -0,0 +1,4 @@
+
+__version__ = "0.1.6"
+
+__all__ = ["Function", "Fitness", "Backtester", "SymbolicRegressor"]
---
+++ setup.py
@@ -0,0 +1,11 @@
+import setuptools
+
+
+setuptools.setup(
+    name="gp_symbolic",
+    description="Genetic Programming for Symbolic Regression",
+    packages=setuptools.find_packages(),
+    classifiers=[
+        "Programming Language :: Python :: 3"
+    ],
+)
